d9177226d9cc248b5541b760c6f523d5089becce
Artizno <nattapon.rakthong@gmail.com>
2024-11-05 13:48:25 +0700

    Add module

--- README.md
+++ README.md
@@ -11,3 +11,9 @@ REPORT_GAS=true npx hardhat test
 npx hardhat node
 npx hardhat ignition deploy ./ignition/modules/Lock.ts
 ```
+
+
+
+npx hardhat ignition deploy ./ignition/modules/MemeBuilder.ts --network bsc_testnet 
+
+npx hardhat verify --network bsc_testnet {{address}} 
---
+++ ignition/modules/MemeBuilder.ts
@@ -0,0 +1,13 @@
+// This setup uses Hardhat Ignition to manage smart contract deployments.
+// Learn more about it at https://hardhat.org/ignition
+
+import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
+
+const MemeBuilderModule = buildModule("MemeBuilderModule", (m) => {
+
+  const memeBuilder = m.contract("MemeBuilder");
+
+  return { memeBuilder };
+});
+
+export default MemeBuilderModule;
